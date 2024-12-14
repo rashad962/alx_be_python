5349166e06c9ac50d4a26d3018b3feb9771358ab
Your Name <rashadaman962@gmail.com>
2024-12-14 19:24:03 +0200

    df

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results in the specified format
+print(f"Sum of {number1} and {number2} is {sum_result}.")
+print(f"Difference of {number1} and {number2} is {difference_result}.")
+print(f"Product of {number1} and {number2} is {product_result}.")
